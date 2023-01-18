027fc23b062bf4e50037b3b8701f5c2c9feab7e5
Aazib Chaudhry <aazibch47@gmail.com>
2023-01-19 00:46:12 +0500

    Setup redux state.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
-import './App.css';
+import { Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from './scenes/HomePage/HomePage';
+import LoginPage from './scenes/LoginPage/LoginPage';
+import ProfilePage from './scenes/ProfilePage/ProfilePage';
 
 function App() {
-    return <div className="app"></div>;
+    return (
+        <div className="app">
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/profile/:userId" element={<ProfilePage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
